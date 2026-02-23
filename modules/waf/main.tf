c71fa20f236531ae310243e38c9010ad8b3e70bf
muhammad-suleman <smsuleman.bukhari@yahoo.com>
2026-02-23 23:49:27 +0000

    adding changes

--- modules/waf/main.tf
+++ modules/waf/main.tf
@@ -1,8 +1,15 @@
 resource "aws_wafv2_web_acl" "waf" {
   name  = "main-waf"
   scope = "REGIONAL"
+
   default_action {
     allow {}
+  }
+
+  visibility_config {
+    cloudwatch_metrics_enabled = true
+    metric_name                = "main-waf"
+    sampled_requests_enabled   = true
   }
 }
 
