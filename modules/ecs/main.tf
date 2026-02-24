0ed18384741581ac4a9a76cea88ade687515d062
muhammad-suleman <smsuleman.bukhari@yahoo.com>
2026-02-24 00:39:57 +0000

    adding changes

--- modules/ecs/main.tf
+++ modules/ecs/main.tf
@@ -37,3 +37,11 @@ resource "aws_ecs_service" "service" {
     container_port   = 80
   }
 }
+
+resource "aws_appautoscaling_target" "ecs" {
+  max_capacity       = var.max_capacity
+  min_capacity       = var.min_capacity
+  resource_id        = "service/${aws_ecs_cluster.cluster.name}/${aws_ecs_service.service.name}"
+  scalable_dimension = "ecs:service:DesiredCount"
+  service_namespace  = "ecs"
+}
